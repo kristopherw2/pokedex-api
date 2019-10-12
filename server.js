b3d624605321d2c76464e7fb60b4bb3b4005bd40
Kristopher Williams <kristopher.williams2@gmail.com>
2019-10-12 09:42:46 -0500

    ready for production

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const POKEDEX = require('./pokedex.json')
 console.log(process.env.API_TOKEN)
 
 const app = express()
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(cors())
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
@@ -53,7 +54,17 @@ app.get('/pokemon', function handleGetPokemon(req, res) {
     res.json(response)
   })
 
-const PORT = 8000
+  app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
